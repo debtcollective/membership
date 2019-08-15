76f82a4b3725421fbe30b89a58b417d2407ea33a
Luis Castro <luiscastroem@gmail.com>
2019-08-15 13:30:13 +0200

    feat(users): add scaffold for model

---
+++ spec/views/users/edit.html.erb_spec.rb
@@ -0,0 +1,30 @@
+require 'rails_helper'
+
+RSpec.describe "users/edit", type: :view do
+  before(:each) do
+    @user = assign(:user, User.create!(
+      :first_name => "MyString",
+      :last_name => "MyString",
+      :email => "MyString",
+      :user_role => "MyString",
+      :discourse_id => "MyString"
+    ))
+  end
+
+  it "renders the edit user form" do
+    render
+
+    assert_select "form[action=?][method=?]", user_path(@user), "post" do
+
+      assert_select "input[name=?]", "user[first_name]"
+
+      assert_select "input[name=?]", "user[last_name]"
+
+      assert_select "input[name=?]", "user[email]"
+
+      assert_select "input[name=?]", "user[user_role]"
+
+      assert_select "input[name=?]", "user[discourse_id]"
+    end
+  end
+end
---
+++ spec/views/users/show.html.erb_spec.rb
@@ -0,0 +1,22 @@
+require 'rails_helper'
+
+RSpec.describe "users/show", type: :view do
+  before(:each) do
+    @user = assign(:user, User.create!(
+      :first_name => "First Name",
+      :last_name => "Last Name",
+      :email => "Email",
+      :user_role => "User Role",
+      :discourse_id => "Discourse"
+    ))
+  end
+
+  it "renders attributes in <p>" do
+    render
+    expect(rendered).to match(/First Name/)
+    expect(rendered).to match(/Last Name/)
+    expect(rendered).to match(/Email/)
+    expect(rendered).to match(/User Role/)
+    expect(rendered).to match(/Discourse/)
+  end
+end
