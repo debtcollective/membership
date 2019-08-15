f934a316cd59b5c4bb5cb4ce43a4a88d1d00acee
Luis Castro <luiscastroem@gmail.com>
2019-08-15 15:32:25 +0200

    test: config shoulda matchers

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -66,3 +66,10 @@ RSpec.configure do |config|
   # FactoryBot
   config.include FactoryBot::Syntax::Methods
 end
+
+Shoulda::Matchers.configure do |config|
+  config.integrate do |with|
+    with.test_framework :rspec
+    with.library :rails
+  end
+end
