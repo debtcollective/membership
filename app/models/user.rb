d127eb77cf563352029c5e9eca9f1b16ae81d08c
Luis Castro <luiscastroem@gmail.com>
2019-08-15 18:30:38 +0200

    style(user): move associations before validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,10 +6,10 @@ class User < ApplicationRecord
   USER_ROLES = { admin: 'admin', user: 'user' }.freeze
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i.freeze
 
+  has_one :subscription
+
   validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false }
   validates :first_name, :last_name, presence: true
-
-  has_one :subscription
 
   def full_name
     "#{first_name} #{last_name}"
