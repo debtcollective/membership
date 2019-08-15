e1450b97a0941e3c300dc88e29ce67fda22673e1
Luis Castro <luiscastroem@gmail.com>
2019-08-15 16:36:56 +0200

    test(plans): add more semantic faker method

--- spec/factories/plans.rb
+++ spec/factories/plans.rb
@@ -2,7 +2,7 @@
 
 FactoryBot.define do
   factory :plan do
-    name { Faker::Lorem.words(number: 4) }
+    name { Faker::Lorem.sentence(word_count: 3) }
     description { Faker::Lorem.paragraph }
     amount { Faker::Number.decimal(l_digits: 2, r_digits: 2) }
   end
