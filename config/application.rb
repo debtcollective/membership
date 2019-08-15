2ada98e156e36229eb3e5634f89c5d70a9651cab
Luis Castro <luiscastroem@gmail.com>
2019-08-15 10:03:12 +0200

    feat: enable uuid as default rails id field

--- config/application.rb
+++ config/application.rb
@@ -33,5 +33,10 @@ module Fundaraising
 
     # Don't generate system test files.
     config.generators.system_tests = nil
+
+    # Use UUID instead of Integer ID
+    config.generators do |g|
+      g.orm :active_record, primary_key_type: :uuid
+    end
   end
 end
