5924f977f8735708cec92f50b192b0679500f977
Luis Castro <luiscastroem@gmail.com>
2019-08-15 13:27:43 +0200

    chore: add faker and FactoryBot syntax methods

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -9,6 +9,7 @@ abort('The Rails environment is running in production mode!') if Rails.env.produ
 require 'rspec/rails'
 # Add additional requires below this line. Rails is not loaded until this point!
 require 'capybara/rspec'
+require 'faker'
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
 # spec/support/ and its subdirectories. Files matching `spec/**/*_spec.rb` are
@@ -61,4 +62,7 @@ RSpec.configure do |config|
   config.filter_rails_from_backtrace!
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
+
+  # FactoryBot
+  config.include FactoryBot::Syntax::Methods
 end
