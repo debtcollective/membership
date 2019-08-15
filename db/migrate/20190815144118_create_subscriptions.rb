441d8806095a7b55991a84e7d6beb3eff0b2772c
Luis Castro <luiscastroem@gmail.com>
2019-08-15 18:18:25 +0200

    fix: missing uuid from subscription  migration

--- db/migrate/20190815144118_create_subscriptions.rb
+++ db/migrate/20190815144118_create_subscriptions.rb
@@ -3,8 +3,8 @@
 class CreateSubscriptions < ActiveRecord::Migration[6.0]
   def change
     create_table :subscriptions, id: :uuid do |t|
-      t.belongs_to :user
-      t.belongs_to :plan
+      t.belongs_to :user, type: :uuid, index: true
+      t.belongs_to :plan, type: :uuid, index: true
       t.boolean :active
 
       t.timestamps
--- db/schema.rb
+++ db/schema.rb
@@ -25,8 +25,8 @@ ActiveRecord::Schema.define(version: 2019_08_15_150836) do
   end
 
   create_table "subscriptions", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
-    t.bigint "user_id"
-    t.bigint "plan_id"
+    t.uuid "user_id"
+    t.uuid "plan_id"
     t.boolean "active"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
