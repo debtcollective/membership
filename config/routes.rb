fbdb8e433b842eea7475dca09233d04b93ccddda
Luis Castro <luiscastroem@gmail.com>
2019-08-19 15:36:30 +0200

    feat(payment): add payment route.

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   resources :plans, only: %i[show index]
   resources :users, only: %i[show new edit update create destroy]
   resources :subscription_first, only: %i[new edit create update]
+  resources :payments, only: %i[new create]
 
   namespace :admin do
     resources :users
