0898b45187d9fd23e86e90e00104e6159e549c4e
Luis Castro <luiscastroem@gmail.com>
2019-08-20 15:30:15 +0200

    feat(cards): add index view for current user

---
+++ app/helpers/user/cards_helper.rb
@@ -0,0 +1,2 @@
+module User::CardsHelper
+end
--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,6 @@ Rails.application.routes.draw do
   post '/card' => 'billings#create_card', as: :create_payment_method
 
   resources :plans, only: %i[show index]
-  resources :users, only: %i[show new edit update create destroy]
   resources :subscription_first, only: %i[new edit create update]
   resources :billings, only: %i[new create]
 
@@ -13,6 +12,10 @@ Rails.application.routes.draw do
     resources :users
     resources :plans
     resources :subscriptions
+  end
+
+  resources :user, only: %i[show new edit update create destroy] do
+    resources :cards, only: %i[index destroy]
   end
 
   root 'static_pages#home'
