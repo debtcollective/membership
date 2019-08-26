f1844b9bb7eaf87841a1a5eded5b36e9fef654c4
Luis Castro <luiscastroem@gmail.com>
2019-08-26 16:05:09 +0200

    feat(users): move table from rails table to material table

--- app/javascript/packs/admin-bundle.js
+++ app/javascript/packs/admin-bundle.js
@@ -1,7 +1,9 @@
 import ReactOnRails from 'react-on-rails';
 
 import Drawer from '../bundles/Admin/components/Drawer';
+import { UsersTable } from '../bundles/Admin/components/Users';
 
 ReactOnRails.register({
   Drawer,
+  UsersTable
 });
