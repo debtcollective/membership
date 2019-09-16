6a3e93549c18049085d8943c38c64e3aeb4d7fdf
Luis Castro <luiscastroem@gmail.com>
2019-09-16 14:36:14 +0200

    test(user): improve feature spec

--- spec/features/user/users_profile_spec.rb
+++ spec/features/user/users_profile_spec.rb
@@ -5,6 +5,7 @@ require 'rails_helper'
 describe 'User - manages their profile', type: :feature, js: true do
   describe 'home' do
     let(:user) { FactoryBot.create(:user) }
+    let(:new_data) { FactoryBot.attributes_for(:user) }
 
     it 'can edit their own data' do
       visit user_path(user)
@@ -14,6 +15,19 @@ describe 'User - manages their profile', type: :feature, js: true do
       expect(page).to have_content(user.email)
 
       click_on 'Edit'
+
+      expect(page).to have_content('Editing User')
+
+      fill_in 'First name', with: new_data[:first_name]
+      fill_in 'Email', with: new_data[:email]
+
+      click_button 'Update User'
+
+      user.reload
+      expect(user.first_name).to eq(new_data[:first_name])
+
+      expect(page).to have_content(user.first_name)
+      expect(page).to have_content(user.last_name)
     end
   end
 end
