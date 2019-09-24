973d214de793474439bef0a11b8cb8af5378bce8
Luis Castro <luiscastroem@gmail.com>
2019-09-24 16:03:45 +0200

    feat: find overdue subscriptions in system

--- app/jobs/find_overdue_subscriptions_job.rb
+++ app/jobs/find_overdue_subscriptions_job.rb
@@ -4,12 +4,12 @@ class FindOverdueSubscriptionsJob < ApplicationJob
   queue_as :default
 
   def perform(*_args)
-    subscriptions_to_charge = subscriptions_with_last_donation.select do |hash|
-      hash[:last_donation] <= 30.days.ago
+    subscriptions_to_charge = subscriptions_with_last_donation.select do |subscription|
+      subscription[:last_donation] ? subscription[:last_donation].created_at <= 30.days.ago : true
     end
 
     subscriptions_to_charge.each do |subscription_to_charge|
-      SubscriptionPaymentJob.perform_later(user: subscription_to_charge.user, plan: subscription_to_charge.plan)
+      SubscriptionPaymentJob.perform_later(user: subscription_to_charge[:user], plan: subscription_to_charge[:plan])
     end
   end
 
@@ -25,19 +25,18 @@ class FindOverdueSubscriptionsJob < ApplicationJob
   end
 
   def last_donation_for(user)
-    p user
     user.donations
         .where(donation_type: Donation::DONATION_TYPES[:subscription])
         .order(created_at: :desc).first
   end
 
   def subscriptions_with_last_donation
-    active_subscriptions.each do |active_subscription|
-      Hash.new(
+    active_subscriptions.map do |active_subscription|
+      {
         user: user_of(active_subscription),
         plan: active_subscription.plan,
         last_donation: last_donation_for(user_of(active_subscription))
-      )
+      }
     end
   end
 end
--- spec/jobs/find_overdue_subscriptions_job_spec.rb
+++ spec/jobs/find_overdue_subscriptions_job_spec.rb
@@ -7,8 +7,8 @@ RSpec.describe FindOverdueSubscriptionsJob, type: :job do
   let!(:inactive_subscription) { FactoryBot.create(:subscription, active: false) }
   let!(:active_subscription_one) { FactoryBot.create(:subscription) }
   let!(:active_subscription_two) { FactoryBot.create(:subscription) }
-  let!(:last_donation_of_recently_paid_subscription) { FactoryBot.create(:donation, user_id: recently_paid_active_subscription.user.id, amount: recently_paid_active_subscription.plan.amount, created_at: 2.weeks.ago) }
-  let!(:last_donation_of_active_subscription_two) { FactoryBot.create(:donation, user_id: active_subscription_two.user.id, amount: active_subscription_two.plan.amount, created_at: 1.month.ago) }
+  let!(:last_donation_of_recently_paid_subscription) { FactoryBot.create(:donation, user_id: recently_paid_active_subscription.user.id, amount: recently_paid_active_subscription.plan.amount, created_at: 2.weeks.ago, donation_type: Donation::DONATION_TYPES[:subscription]) }
+  let!(:last_donation_of_active_subscription_two) { FactoryBot.create(:donation, user_id: active_subscription_two.user.id, amount: active_subscription_two.plan.amount, created_at: 1.month.ago, donation_type: Donation::DONATION_TYPES[:subscription]) }
 
   subject(:job) { described_class.perform_later }
 
@@ -23,7 +23,7 @@ RSpec.describe FindOverdueSubscriptionsJob, type: :job do
 
   it 'executes perform' do
     expect do
-      perform_enqueued_jobs { job }
+      described_class.perform_now
     end.to change(ActiveJob::Base.queue_adapter.enqueued_jobs, :size).by(2)
   end
 
