27e7ef962e15d973670a4abe5736cbab94d1e632
Luis Castro <luiscastroem@gmail.com>
2019-10-02 16:59:50 +0200

    feat: admin constraint

--- lib/admin_constraint.rb
+++ lib/admin_constraint.rb
@@ -10,16 +10,10 @@ class AdminConstraint
   def matches?(request)
     return false if @require_master && RailsMultisite::ConnectionManagement.current_db != 'default'
 
-    provider = Discourse.current_user_provider.new(request.env)
+    provider = Fundraising.session_provider.new(request.env)
     provider.current_user&.admin? &&
       custom_admin_check(request)
-  rescue Discourse::InvalidAccess, Discourse::ReadOnly
+  rescue Fundraising::InvalidAccess, Fundraising::ReadOnly
     false
-  end
-
-  # Extensibility point: plugins can overwrite this to add additional checks
-  # if they require.
-  def custom_admin_check(_request)
-    true
   end
 end
