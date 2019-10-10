82bd4e854ee3e966eb0e7ee507b5b1e53bab5f4c
Orlando Del Aguila <orlando@hashlabs.com>
2019-10-10 16:53:29 -0500

    dev: add full login/signup url to env variables

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,19 +3,19 @@
 class SessionsController < ApplicationController
   def login
     query_string = redirect_params.to_query
-    url = "#{ENV['DISCOURSE_URL']}/session/sso_provider?#{query_string}"
+    url = "#{ENV['DISCOURSE_LOGIN_URL']}?#{query_string}"
 
     redirect_to url
   end
 
   def signup
     query_string = redirect_params.to_query
-    url = "#{ENV['DISCOURSE_URL']}/session/sso_provider/signup?#{query_string}"
+    url = "#{ENV['DISCOURSE_SIGNUP_URL']}?#{query_string}"
 
     redirect_to url
   end
 
   def redirect_params
-    params.permit(:return_url).merge(return_url: root_url)
+    params.permit(:return_url).reverse_merge(return_url: root_url)
   end
 end
