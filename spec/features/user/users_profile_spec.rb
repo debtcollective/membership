28f7ff73634e58491a4d754da7f50e004210d13b
Luis Castro <luiscastroem@gmail.com>
2019-10-14 14:01:01 +0200

    test: add subscription cancel from user profile

--- spec/features/user/users_profile_spec.rb
+++ spec/features/user/users_profile_spec.rb
@@ -5,7 +5,7 @@ require 'rails_helper'
 describe 'User - manages their profile', type: :feature, js: true do
   describe 'home' do
     let!(:user) { FactoryBot.create(:user) }
-    let!(:subscription) { FactoryBot.create(:subscription, user_id: user.id) }
+    let!(:subscription) { FactoryBot.create(:subscription, user_id: user.id, active: true) }
     let!(:one_time_donations) { FactoryBot.create_list(:donation, 5, user_id: user.id, donation_type: Donation::DONATION_TYPES[:one_off]) }
 
     before(:each) do
@@ -26,6 +26,26 @@ describe 'User - manages their profile', type: :feature, js: true do
       user.donations.each do |donation|
         expect(page).to have_content(donation.amount)
       end
+    end
+
+    it 'can cancel an active subscription' do
+      expect(subscription.active).to eq(true)
+      visit user_path(user)
+      expect(page).to have_content(user.name)
+      expect(page).to have_content(user.email)
+
+      expect(page).to have_content(subscription.plan.name)
+
+      click_button 'Cancel Subscription'
+
+      expect(page).to have_content('Do you want to terminate your current subscription?')
+      within '#cancel-subscription-dialog' do
+        click_button 'Cancel Subscription'
+      end
+
+      sleep 2
+      subscription.reload
+      expect(subscription.active).to eq(false)
     end
   end
 end
