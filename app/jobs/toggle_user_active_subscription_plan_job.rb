039a6e473d49d4901715de00219dd851b4db550f
Luis Castro <luiscastroem@gmail.com>
2019-10-30 10:09:32 +0100

    feat: wrap subscription toggling with an active record transaction

--- app/jobs/toggle_user_active_subscription_plan_job.rb
+++ app/jobs/toggle_user_active_subscription_plan_job.rb
@@ -9,18 +9,20 @@ class ToggleUserActiveSubscriptionPlanJob < ApplicationJob
 
     # disable current user subscription.
     old_subscription = user.active_subscription
-    old_subscription.update(active: false)
+    ActiveRecord::Base.transaction do
+      old_subscription.update(active: false)
 
-    # creates a new subscription with the new plan details.
-    new_subscription = Subscription.new(
-      user_id: user_plan_change.user_id,
-      plan_id: user_plan_change.new_plan_id,
-      last_charge_at: old_subscription.last_charge_at,
-      active: true
-    )
+      # creates a new subscription with the new plan details.
+      new_subscription = Subscription.new(
+        user_id: user_plan_change.user_id,
+        plan_id: user_plan_change.new_plan_id,
+        last_charge_at: old_subscription.last_charge_at,
+        active: true
+      )
 
-    if new_subscription.save
-      UserPlanChange.find(user_plan_change.id).update(status: 'finished')
+      if new_subscription.save
+        UserPlanChange.find(user_plan_change.id).update(status: 'finished')
+      end
     end
   end
 end
