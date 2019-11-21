ca4af38ea4346bceb966f9f671a763f800cd9056
Luis Castro <luiscastroem@gmail.com>
2019-11-21 12:42:15 +0100

    chore: add recaptcha initializer

---
+++ config/initializers/recaptcha.rb
@@ -0,0 +1,6 @@
+# frozen_string_literal: true
+
+Recaptcha.configure do |config|
+  config.site_key = '6Lc6BAAAAAAAAChqRbQZcn_yyyyyyyyyyyyyyyyy'
+  config.secret_key = '6Lc6BAAAAAAAAKN3DRm6VA_xxxxxxxxxxxxxxxxx'
+end
