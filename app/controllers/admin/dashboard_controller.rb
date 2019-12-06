277a33ad6e9cc5b1ffa16d5056ecf9661c2cc011
Luis Castro <luiscastroem@gmail.com>
2019-12-06 14:41:51 +0100

    feat(dashboard): add controller with metrics

---
+++ app/controllers/admin/dashboard_controller.rb
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+
+class Admin::DashboardController < AdminController
+  before_action -> { current_page_title('Dashboard') }
+
+  def index
+    @active_subscriptions_count = Subscription.where(active: true).count
+    subscriptions = Donation.where(status: 'finished', donation_type: Donation::DONATION_TYPES[:subscription])
+    @amount_from_subscriptions = subscriptions.collect(&:amount).sum.to_f
+
+    one_off_donations = Donation.where(status: 'finished', donation_type: Donation::DONATION_TYPES[:one_off])
+    @donations_count = one_off_donations.count
+    @amount_from_donations = one_off_donations.collect(&:amount).sum.to_f
+  end
+end
