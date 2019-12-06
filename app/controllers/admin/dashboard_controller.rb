b9aaf48011b5906a81686ac37a9980c451655b30
Luis Castro <luiscastroem@gmail.com>
2019-12-06 18:12:01 +0100

    refactor(dashboard): counts to use sum instead of collect

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -6,10 +6,10 @@ class Admin::DashboardController < AdminController
   def index
     @active_subscriptions_count = Subscription.where(active: true).count
     subscriptions = Donation.where(status: 'finished', donation_type: Donation::DONATION_TYPES[:subscription])
-    @amount_from_subscriptions = subscriptions.collect(&:amount).sum.to_f
+    @amount_from_subscriptions = subscriptions.sum(&:amount).to_f
 
     one_off_donations = Donation.where(status: 'finished', donation_type: Donation::DONATION_TYPES[:one_off])
     @donations_count = one_off_donations.count
-    @amount_from_donations = one_off_donations.collect(&:amount).sum.to_f
+    @amount_from_donations = one_off_donations.sum(&:amount).to_f
   end
 end
