d0062e6a6074d3eb780a730045e4b34e11668696
Orlando Del Aguila <orlando@hashlabs.com>
2019-12-19 13:06:46 -0600

    dev: add sidekiq-scheduler web integration

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 require 'sidekiq/web'
+require 'sidekiq-scheduler/web'
 
 Rails.application.routes.draw do
   get '/card/new' => 'billings#new_card', as: :add_payment_method
