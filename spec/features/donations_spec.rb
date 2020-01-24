e76cf525d3414bdfecbdc5c07abf87cd7dd05702
Luis Castro <luiscastroem@gmail.com>
2020-01-24 19:52:11 +0100

    test(donations): add spec for less than $5 donations

--- spec/features/donations_spec.rb
+++ spec/features/donations_spec.rb
@@ -27,6 +27,31 @@ describe 'Donations', type: :feature do
       end
     end
 
+    it 'fails when trying to donate less than $5' do
+      allow_any_instance_of(SessionProvider).to receive(:current_user).and_return(user)
+      visit '/'
+      expect(page).to_not have_content('Log In') # checking user is logged in
+      expect(page).to have_content('Pay what you can')
+
+      click_link 'one-time-donation'
+
+      expect(page).to have_content('Pay what you can. Every dollar counts.')
+
+      within '.one-time-donation' do
+        fill_stripe_elements(card: '4242424242424242')
+        fill_in 'amount-field', with: 4
+        expect(page).to have_button('Make my donation', disabled: true)
+        fill_in 'amount-field', with: 5
+        expect(page).to have_button('Make my donation', disabled: false)
+        fill_in 'amount-field', with: 2
+        expect(page).to have_button('Make my donation', disabled: true)
+      end
+
+      using_wait_time(10) do
+        expect(page).to_not have_content('Thank you for donating $4.00')
+      end
+    end
+
     it 'notifies when the transaction was declined' do
       allow_any_instance_of(SessionProvider).to receive(:current_user).and_return(user)
       visit '/'
