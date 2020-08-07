b353e13bd7eb792c4b79fbfd54a52b5e669c757b
Orlando Del Aguila <orlando@hashlabs.com>
2020-08-07 17:49:33 -0500

    dev: add thank-you page

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -2,7 +2,6 @@
 
 class StaticPagesController < ApplicationController
   def home
-    flash[:info] = 'te amo'
     @current_user = current_user
     @current_user_subscription = @current_user&.active_subscription
     @plans = Plan.all.order('amount asc')
