630c21ddb43ea10eeff3a9ed520366d53cda8bf6
Orlando Del Aguila <orlando@hashlabs.com>
2020-08-19 10:53:56 -0500

    dev: remove invalid gifts copy from plans description

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,30 +10,26 @@
 [
   {
     amount: 5,
-    name: 'Basic',
-    headline: '	Help us fund our mission.',
+    name: "Basic",
+    headline: "\tHelp us fund our mission.",
     description: <<~HEREDOC
       This monthly donation helps keep our platform up and running and our team working one-on-one with folks in debt.
-
-      Basic members will receive a badge for their profile on our platform.
     HEREDOC
   },
   {
     amount: 20,
-    name: 'Sustaining',
-    headline: 'Push us closer to future projects.',
+    name: "Sustaining",
+    headline: "Push us closer to future projects.",
     description: <<~HEREDOC
       This level helps us plan for future actions and initiatives, as well as grow and develop new strategies only possible with the right level of funds.
-
-      Sustaining members will receive a special badge for their profile on our community platform.
     HEREDOC
   }
 ].each do |data|
-  plan = Plan.find_or_initialize_by(name: data[:name]) do |p|
+  plan = Plan.find_or_initialize_by(name: data[:name]) { |p|
     p.amount = data[:amount]
     p.headline = data[:headline]
     p.description = data[:description]
-  end
+  }
 
   plan.save
 end
