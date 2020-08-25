e5274ee09d77b5710fdb0028655a4febea2e6cd7
Orlando Del Aguila <orlando@hashlabs.com>
2020-08-24 20:18:00 -0500

    dev: add port to mailer.asset_host

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -72,5 +72,5 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {address: "127.0.0.1", port: 1025}
   config.action_mailer.raise_delivery_errors = false
-  config.action_mailer.asset_host = "http://#{ENV["DEV_HOST"]}"
+  config.action_mailer.asset_host = "http://#{ENV["DEV_HOST"]}:#{ENV["DEV_PORT"]}"
 end
