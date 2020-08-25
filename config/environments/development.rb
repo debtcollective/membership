9b204d09a3a62e3227d1816a361fd48034e72a9b
Orlando Del Aguila <orlando@hashlabs.com>
2020-08-24 19:57:42 -0500

    dev: validate phone_number minlength, add asset_host to mailer

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -72,4 +72,5 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {address: "127.0.0.1", port: 1025}
   config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.asset_host = "http://#{ENV["DEV_HOST"]}"
 end
