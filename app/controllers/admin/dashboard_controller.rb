40147ea8f7416c77f819fd7e2f2c934948ae83b3
Orlando Del Aguila <orlando@hashlabs.com>
2020-09-01 16:05:37 -0500

    dev(admin): use SQL to sum donation amounts

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,15 +1,22 @@
 # frozen_string_literal: true
 
 class Admin::DashboardController < AdminController
-  before_action -> { current_page_title("Dashboard") }
+  before_action -> { current_page_title('Dashboard') }
 
   def index
     @active_subscriptions_count = Subscription.where(active: true).count
-    subscriptions = Donation.where(status: "succeeded", donation_type: Donation::DONATION_TYPES[:subscription])
-    @amount_from_subscriptions = subscriptions.sum(&:amount).to_f
+    @amount_from_subscriptions =
+      Donation.where(
+        status: 'succeeded',
+        donation_type: Donation::DONATION_TYPES[:subscription]
+      ).sum(:amount)
 
-    one_off_donations = Donation.where(status: "succeeded", donation_type: Donation::DONATION_TYPES[:one_off])
+    one_off_donations =
+      Donation.where(
+        status: 'succeeded', donation_type: Donation::DONATION_TYPES[:one_off]
+      )
+
     @donations_count = one_off_donations.count
-    @amount_from_donations = one_off_donations.sum(&:amount).to_f
+    @amount_from_donations = one_off_donations.sum(:amount)
   end
 end
