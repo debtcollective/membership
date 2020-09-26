dd9893ea84a4655fd2a907c20bf355188f3e5b4f
Orlando Del Aguila <orlando@hashlabs.com>
2020-09-26 14:28:46 -0500

    dev(session): session provider should use session to find user along cookies

--- app/services/session_provider.rb
+++ app/services/session_provider.rb
@@ -1,33 +1,42 @@
 # frozen_string_literal: true
 
 class SessionProvider
-  attr_accessor :cookies
+  attr_accessor :cookies, :session
 
-  def initialize(cookies)
+  def initialize(cookies, session)
     @cookies = cookies
+    @session = session
   end
 
   def current_user
-    return nil unless sso_cookie_present?
+    if sso_cookie_present?
+      payload, headers = sso_jwt_content
 
-    payload, headers = sso_jwt_content
+      user, new_record = User.find_or_create_from_sso(payload)
 
-    user, new_record = User.find_or_create_from_sso(payload)
+      current_user = CurrentUser.new(user, payload, new_record)
+    elsif user_id = session[:user_id]
+      user = User.find(user_id)
+      new_record = user.new_record?
+      payload = {}
 
-    CurrentUser.new(user, payload, new_record)
+      current_user = CurrentUser.new(user, payload, new_record)
+    end
+
+    current_user
   end
 
   private
 
   def sso_jwt_content
-    token = cookies[ENV['SSO_COOKIE_NAME']]
+    token = cookies[ENV["SSO_COOKIE_NAME"]]
 
     # this may throw JWT::VerificationError
     # let those calling current_user handle the error
-    JWT.decode(token, ENV['SSO_JWT_SECRET'], true, algorithm: 'HS256')
+    JWT.decode(token, ENV["SSO_JWT_SECRET"], true, algorithm: "HS256")
   end
 
   def sso_cookie_present?
-    cookies[ENV['SSO_COOKIE_NAME']].present?
+    cookies[ENV["SSO_COOKIE_NAME"]].present?
   end
 end
--- spec/services/session_provider_spec.rb
+++ spec/services/session_provider_spec.rb
@@ -1,20 +1,44 @@
 # frozen_string_literal: true
 
-require 'rails_helper'
+require "rails_helper"
 
 RSpec.describe SessionProvider, type: :service do
-  describe '#current_user' do
-    it 'reads cookie and returns current_user' do
-      ClimateControl.modify(SSO_COOKIE_NAME: 'tdc_auth_cookie', SSO_JWT_SECRET: 'jwt-secret') do
-        payload = JSON.parse(file_fixture('jwt_sso_payload.json').read)
-        jwt = JWT.encode(payload, ENV['SSO_JWT_SECRET'], 'HS256')
-        cookie_name = ENV['SSO_COOKIE_NAME']
+  describe "#current_user" do
+    it "reads cookie and returns current_user" do
+      ClimateControl.modify(SSO_COOKIE_NAME: "tdc_auth_cookie", SSO_JWT_SECRET: "jwt-secret") do
+        payload = JSON.parse(file_fixture("jwt_sso_payload.json").read)
+        jwt = JWT.encode(payload, ENV["SSO_JWT_SECRET"], "HS256")
+        cookie_name = ENV["SSO_COOKIE_NAME"]
         cookies = {}
+        session = {}
         cookies[cookie_name] = jwt
 
-        current_user = SessionProvider.new(cookies).current_user
+        current_user = SessionProvider.new(cookies, session).current_user
 
         expect(current_user.external_id).to eql(1)
+      end
+    end
+
+    it "returns user from session if cookies are not available" do
+      ClimateControl.modify(SSO_COOKIE_NAME: "tdc_auth_cookie", SSO_JWT_SECRET: "jwt-secret") do
+        user = FactoryBot.create(:user)
+        cookies = {}
+        session = {user_id: user.id}
+
+        current_user = SessionProvider.new(cookies, session).current_user
+
+        expect(current_user.id).to eql(user.id)
+      end
+    end
+
+    it "returns nil if no user was found" do
+      ClimateControl.modify(SSO_COOKIE_NAME: "tdc_auth_cookie", SSO_JWT_SECRET: "jwt-secret") do
+        cookies = {}
+        session = {}
+
+        current_user = SessionProvider.new(cookies, session).current_user
+
+        expect(current_user).to be_nil
       end
     end
   end
