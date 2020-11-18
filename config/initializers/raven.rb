9e924b379e4012b1d1835967a81eaa9c490e2a2e
Orlando Del Aguila <orlando@hashlabs.com>
2020-11-18 12:42:25 -0600

    dev: add sentry_environment

--- config/initializers/raven.rb
+++ config/initializers/raven.rb
@@ -2,5 +2,6 @@
 
 # Initializes sentry logging on rails
 Raven.configure do |config|
-  config.dsn = ENV['SENTRY_DSN']
+  config.dsn = ENV["SENTRY_DSN"]
+  config.current_environment = ENV["SENTRY_ENVIRONMENT"] || "development"
 end
