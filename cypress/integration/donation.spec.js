89be7f210b43915e17b3645519d576d7cbbca043
Orlando Del Aguila <orlando@hashlabs.com>
2021-01-07 14:53:59 -0600

    test: add cypress donation spec

--- cypress/integration/donation.spec.js
+++ cypress/integration/donation.spec.js
@@ -5,6 +5,34 @@ describe('Donation Spec', () => {
 
   it('renders donation widget', () => {
     cy.visit('/test/widget/donation')
-    cy.contains('Yay, you saved a compliment!')
+    cy.contains('DONATE', { matchCase: false })
+  })
+
+  it('makes a donation', () => {
+    cy.visit('/test/widget/donation')
+
+    // select amount
+    cy.contains('$10').click()
+    cy.get('button')
+      .contains('DONATE', { matchCase: false })
+      .click()
+
+    // fill address
+    cy.contains('PAYING 10$', { matchCase: false }).click()
+
+    cy.get("input[name='address']").type('te amo')
+    cy.get("input[name='city']").type('te amo')
+    cy.get("input[name='zipCode']").type('te amo')
+    cy.get("select[name='country']").select('MX')
+
+    cy.get('button')
+      .contains('NEXT STEP', { matchCase: false })
+      .click()
+
+    // complete card information
+    cy.get("input[name='address']").type('te amo')
+    cy.get("input[name='city']").type('te amo')
+
+    // assert thank you screen
   })
 })
