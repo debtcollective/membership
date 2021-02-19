4f7591cff8495ae5fd88b119162390013aa12920
Orlando Del Aguila <orlando@hashlabs.com>
2021-02-19 16:33:46 -0600

    dev(email_token): add view to confirm user email_token
    to prevent email client bots to confirm the account instead of the user

--- app/controllers/user_confirmations_controller.rb
+++ app/controllers/user_confirmations_controller.rb
@@ -49,9 +49,23 @@ class UserConfirmationsController < ApplicationController
     end
   end
 
-  # GET /user_confirmations/confirm/:email_token
+  # GET /user_confirmations/confirm_email/:email_token
   def confirm_email_token
     @user = User.find_by_email_token!(params[:email_token])
+
+    respond_to do |format|
+      if @user
+        format.html { render :index, status: :ok }
+      else
+        format.html { render :index, status: :not_found }
+      end
+    end
+  end
+
+  # POST /user_confirmations/confirm_email
+  def confirm_email
+    @email_token = user_confirmation_params[:email_token]
+    @user = User.find_by_email_token!(@email_token)
 
     respond_to do |format|
       if @user&.activate!
@@ -60,6 +74,13 @@ class UserConfirmationsController < ApplicationController
         format.html { redirect_to email_login_url }
         format.json { render json: {status: "success", message: "Email confirmed", redirect_url: email_login_url}, status: :ok }
       else
+        Raven.capture_message(
+          "Couldn't activate user with email_token",
+          extra: {
+            email_token: @email_token
+          }
+        )
+
         format.html { render plain: "We couldn't activate your account. If this is an error please contact support at https://debtcollective.org", status: :internal_server_error }
         format.json { render json: {status: "failed", message: "Invalid confirmation token"}, status: :not_found }
       end
