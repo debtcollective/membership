f7b9e056f7a19855b1adcbd3cb9cf0d5fa3bf873
Orlando Del Aguila <orlando@hashlabs.com>
2021-02-23 15:16:10 -0600

    test(email_token): add integration testing for email validation

---
+++ cypress/integration/email-confirmation.spec.js
@@ -0,0 +1,27 @@
+import faker from 'faker'
+
+describe('Membership Spec', () => {
+  beforeEach(() => {
+    cy.request('/cypress_rails_reset_state')
+  })
+
+  describe('happy', () => {
+    it('renders the confirmation page and user cofirms the email', () => {
+      cy.appFactories([['create', 'user_with_email_token', {}]]).then(
+        records => {
+          const user = records[0]
+          const redirectUrl = `http://lvh.me:3000/session/email-login/${user.email_token}`
+          cy.visit(`/user_confirmations/confirm_email/${user.email_token}`)
+
+          // assert the redirect
+          cy.intercept(redirectUrl, req => {
+            expect(req.url).to.eq(redirectUrl)
+            req.reply({ status: 200, body: `redirected to ${redirectUrl}` })
+          })
+
+          cy.contains('Confirm my email', { matchCase: false }).click()
+        }
+      )
+    })
+  })
+})
--- spec/controllers/user_confirmations_controller_spec.rb
+++ spec/controllers/user_confirmations_controller_spec.rb
@@ -82,4 +82,23 @@ RSpec.describe UserConfirmationsController, type: :controller do
       expect { get :confirm_email_token, params: {email_token: SecureRandom.hex(20)} }.to raise_error(ActionController::RoutingError)
     end
   end
+
+  describe "POST #confirm_email" do
+    it "confirms user email if email token is valid" do
+      user = FactoryBot.create(:user_with_email_token)
+
+      post :confirm_email, params: {user_confirmation: {email_token: user.email_token}}
+      user.reload
+
+      expect(response.status).to eq(302)
+      expect(user.active?).to eq(true)
+      expect(user.activated_at).to be_within(1.second).of DateTime.now
+    end
+
+    it "returns not found if token is invalid" do
+      expect {
+        post :confirm_email, params: {user_confirmation: {email_token: SecureRandom.hex(20)}}
+      }.to raise_error(ActionController::RoutingError)
+    end
+  end
 end
