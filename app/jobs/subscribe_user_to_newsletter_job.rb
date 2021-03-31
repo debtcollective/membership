3d86b75307bf405f6ff83db35a3e49b80550f15b
Orlando Del Aguila <orlando@hashlabs.com>
2021-03-31 15:52:38 -0600

    feat(mailchimp): include address when subscribing members to newsletter

--- app/jobs/subscribe_user_to_newsletter_job.rb
+++ app/jobs/subscribe_user_to_newsletter_job.rb
@@ -12,7 +12,6 @@ class SubscribeUserToNewsletterJob < ApplicationJob
     user = User.find(user_id)
     email = user.email
     customer_ip = user.custom_fields["customer_ip"]
-    phone_number = user.phone_number
 
     gibbon = Gibbon::Request.new(api_key: api_key, debug: debug)
     email_digest = Digest::MD5.hexdigest(email)
@@ -21,17 +20,8 @@ class SubscribeUserToNewsletterJob < ApplicationJob
       email_address: email,
       status: "subscribed",
       ip_signup: customer_ip,
-      merge_fields: {
-        FNAME: user.first_name,
-        LNAME: user.last_name
-      }
+      merge_fields: merge_fields(user)
     }
-
-    # TODO: validation until we implement a better one on the union widget/membership controller
-    #   Mailchimp fails if the phone number is not valid
-    if phone_number.length >= 8
-      newsletter_params[:merge_fields][:PHONE] = phone_number
-    end
 
     gibbon
       .lists(list_id)
@@ -47,7 +37,44 @@ class SubscribeUserToNewsletterJob < ApplicationJob
           tags: tags
         })
     end
+
+    user.custom_fields["subscribed_to_newsletter"] = true
+
+    user.save
   rescue Gibbon::MailChimpError => e
     Raven.capture_exception(e)
+  end
+
+  def merge_fields(user)
+    merge_fields_hash = {
+      FNAME: user.first_name,
+      LNAME: user.last_name
+    }
+
+    # Address merge field has a specific schema
+    # https://us1.api.mailchimp.com/schema/3.0/Lists/Members/MergeField.json
+    custom_fields = user.custom_fields
+    address_keys = [:addr1, :city, :country, :state, :zip]
+    address = {
+      addr1: custom_fields["address_line1"],
+      city: custom_fields["address_city"],
+      country: custom_fields["address_country_code"],
+      state: custom_fields.fetch("address_state", "n/a"),
+      zip: custom_fields["address_zip"]
+    }
+
+    valid_address = address_keys.all? { |key| address[key].present? }
+    if valid_address
+      merge_fields_hash[:ADDRESS] = address
+    end
+
+    # TODO: validation until we implement a better one on the union widget/membership controller
+    # Mailchimp fails if the phone number is not valid
+    phone_number = user.phone_number
+    if phone_number.length >= 8
+      merge_fields_hash[:PHONE] = phone_number
+    end
+
+    merge_fields_hash
   end
 end
