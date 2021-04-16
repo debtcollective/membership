bf2321635000db39ce7241fa78495f15b414d611
Orlando Del Aguila <orlando@hashlabs.com>
2021-04-16 13:45:19 -0500

    dev(phone-number-field): make css look like the rest of the inputs

--- app/form_builders/application_form_builder.rb
+++ app/form_builders/application_form_builder.rb
@@ -2,16 +2,15 @@ class ApplicationFormBuilder < ActionView::Helpers::FormBuilder
   include React::Rails::ViewHelper
 
   def phone_number_field(method, opts = {})
-    default_opts = {className: "mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 #{"border-2 border-red-500" if @object.errors.any?}"}
-    props = {
+    default_opts = {
       name: "#{@object_name}[#{method}]",
       method: method,
       value: @object.respond_to?(method) ? @object.send(method) : nil,
       errors: @object.errors.to_hash.fetch(method, nil)
     }
-    merged_opts = default_opts.merge(opts).merge(props)
+    props = default_opts.merge(opts)
 
-    prerender_component("PhoneNumberField", merged_opts)
+    prerender_component("PhoneNumberField", props)
   end
 
   def prerender_component(name, props)
