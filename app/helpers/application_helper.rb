ed0d39fd9689c6bc04bdce510493c738371b342d
Orlando Del Aguila <orlando@hashlabs.com>
2021-04-28 19:21:54 -0500

    feat(alerts): add alert messages

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -11,18 +11,15 @@ module ApplicationHelper
     end
   end
 
-  def flash_messages(_opts = {})
-    flash.each do |msg_type, message|
-      concat(
-        content_tag(:div, message, class: "alert alert-#{msg_type}", role: "alert") {
-          concat message
-          concat content_tag(:button, content_tag(:span, "×", data: {'aria-hidden': "true"}),
-            class: "close", data: {dismiss: "alert"})
-        }
-      )
-    end
+  def flash_message_class(type)
+    @flash_message_classes ||= {
+      error: "red",
+      info: "blue",
+      success: "green",
+      warning: "yellow"
+    }
 
-    nil
+    @flash_message_classes[type.to_sym]
   end
 
   # current_path? returns false on POST request, we use this one instead.
