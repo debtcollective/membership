89d9c2838a73e6a947670cb7683c49e0ddaeb27b
Orlando Del Aguila <orlando@hashlabs.com>
2021-05-10 18:50:37 -0500

    feat(update-card): add loading state for submit button

--- app/javascript/components/UpdateCreditCardForm/index.jsx
+++ app/javascript/components/UpdateCreditCardForm/index.jsx
@@ -34,6 +34,46 @@ const CreditCardField = ({ onChange }) => {
   )
 }
 
+const SubmitButton = ({ isLoading }) => {
+  const label = isLoading ? (
+    <>
+      <svg
+        className='w-5 h-5 mr-3 -ml-1 text-white animate-spin'
+        xmlns='http://www.w3.org/2000/svg'
+        fill='none'
+        viewBox='0 0 24 24'
+        width='1em'
+        height='1em'
+      >
+        <circle
+          className='opacity-25'
+          cx={12}
+          cy={12}
+          r={10}
+          stroke='currentColor'
+          strokeWidth={4}
+        />
+        <path
+          className='opacity-75'
+          fill='currentColor'
+          d='M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z'
+        />
+      </svg>
+      Saving
+    </>
+  ) : (
+    <>Save</>
+  )
+  return (
+    <button
+      disabled={isLoading}
+      className='inline-flex justify-center px-4 py-2 ml-3 text-sm font-medium text-white bg-indigo-600 border border-transparent rounded-md shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500'
+    >
+      {label}
+    </button>
+  )
+}
+
 const UpdateCreditCardForm = ({
   action,
   method,
@@ -149,12 +189,7 @@ const UpdateCreditCardForm = ({
 
       <div className='pt-5'>
         <div className='flex justify-end'>
-          <button
-            disabled={isLoading}
-            className='inline-flex justify-center px-4 py-2 ml-3 text-sm font-medium text-white bg-indigo-600 border border-transparent rounded-md shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500'
-          >
-            Save
-          </button>
+          <SubmitButton isLoading={isLoading} />
         </div>
       </div>
     </form>
