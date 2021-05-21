6be5578893e65253abfa2e1e99092d3eebcc6f7c
Orlando Del Aguila <orlando@hashlabs.com>
2021-05-21 18:44:36 -0500

    dev: add paper_trail to track changes on subscription model

---
+++ db/migrate/20210522002128_add_object_changes_to_versions.rb
@@ -0,0 +1,5 @@
+class AddObjectChangesToVersions < ActiveRecord::Migration[6.1]
+  def change
+    add_column :versions, :object_changes, :jsonb
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_19_195922) do
+ActiveRecord::Schema.define(version: 2021_05_22_002128) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -150,6 +150,17 @@ ActiveRecord::Schema.define(version: 2021_05_19_195922) do
     t.string "email_token"
     t.inet "registration_ip_address"
     t.index ["email"], name: "index_users_on_email", unique: true
+  end
+
+  create_table "versions", force: :cascade do |t|
+    t.string "item_type", null: false
+    t.bigint "item_id", null: false
+    t.string "event", null: false
+    t.string "whodunnit"
+    t.jsonb "object"
+    t.datetime "created_at"
+    t.jsonb "object_changes"
+    t.index ["item_type", "item_id"], name: "index_versions_on_item_type_and_item_id"
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
