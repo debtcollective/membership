15094ca163532993cf7d84ca6f3cd25fe5d446ab
Orlando Del Aguila <orlando@hashlabs.com>
2021-05-26 14:14:20 -0500

    dev: rename subscriptions job

--- app/jobs/find_due_subscriptions_job.rb
+++ app/jobs/find_due_subscriptions_job.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-class FindOverdueSubscriptionsJob < ApplicationJob
+class FindDueSubscriptionsJob < ApplicationJob
   queue_as :default
 
   def perform
--- spec/jobs/find_due_subscriptions_job_spec.rb
+++ spec/jobs/find_due_subscriptions_job_spec.rb
@@ -2,7 +2,7 @@
 
 require "rails_helper"
 
-RSpec.describe FindOverdueSubscriptionsJob, type: :job do
+RSpec.describe FindDueSubscriptionsJob, type: :job do
   let!(:recently_paid_active_subscription) { FactoryBot.create(:subscription, status: :active, last_charge_at: 2.weeks.ago) }
   let!(:inactive_subscription) { FactoryBot.create(:subscription, status: :inactive) }
   let!(:due_subscription_one) { FactoryBot.create(:subscription, status: :active, last_charge_at: (Subscription::SUBSCRIPTION_PERIOD + 1.day).ago) }
@@ -10,16 +10,16 @@ RSpec.describe FindOverdueSubscriptionsJob, type: :job do
   let!(:free_subscription) { FactoryBot.create(:subscription, amount: 0, status: :active, last_charge_at: nil) }
 
   it "queues the job" do
-    expect { FindOverdueSubscriptionsJob.perform_later }
+    expect { FindDueSubscriptionsJob.perform_later }
       .to change(ActiveJob::Base.queue_adapter.enqueued_jobs, :size).by(1)
   end
 
   it "is in default queue" do
-    expect(FindOverdueSubscriptionsJob.new.queue_name).to eq("default")
+    expect(FindDueSubscriptionsJob.new.queue_name).to eq("default")
   end
 
   it "executes perform" do
-    expect { FindOverdueSubscriptionsJob.perform_now }
+    expect { FindDueSubscriptionsJob.perform_now }
       .to change(ActiveJob::Base.queue_adapter.enqueued_jobs, :size).by(2)
   end
 
