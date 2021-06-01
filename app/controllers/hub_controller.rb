88d7a8d1415b401e56da0ddb23888d8c415d00fd
Orlando Del Aguila <orlando@hashlabs.com>
2021-05-31 19:32:43 -0500

    dev: allow to see the membership hub for all users

--- app/controllers/hub_controller.rb
+++ app/controllers/hub_controller.rb
@@ -1,14 +1,3 @@
 class HubController < ApplicationController
   before_action :authenticate_user!
-  before_action :allowed_to_view?
-
-  def allowed_to_view?
-    return true if current_user.email&.ends_with?("@debtcollective.org")
-
-    if request.get?
-      redirect_to_home_page
-    else
-      head :unauthorized
-    end
-  end
 end
